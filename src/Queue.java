386e1c0fd8a3e7543e9a15bdeb0b886c93dd1e41
jgecko11 <jgreco22@kentdenver.org>
2020-11-12 11:22:02 -0700

    test commit

---
+++ src/Queue.java
@@ -0,0 +1,28 @@
+public class Queue <E>{
+
+    private E data;
+
+    public Queue(){
+
+    }
+
+    public void enqueue(E element){ //add element
+
+    }
+
+    public E dequeue() { //remove and return the least recent element
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+    public int size(){
+
+    }
+
+    public E peek(){ //returns the least recent but does not remove
+
+    }
+}
