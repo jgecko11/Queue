5b1848503ddade3f0ed4e456dbfe76b367ab7cec
jgecko11 <jgreco22@kentdenver.org>
2020-11-17 10:16:03 -0700

    QUEUE!

--- src/Queue.java
+++ src/Queue.java
@@ -1,28 +1,57 @@
-public class Queue <E>{
+import java.util.NoSuchElementException;
+//@param <T>
+public class Queue <T>{
 
-    private E data;
+    private int size;
+    private QueueNode first;
+    private QueueNode last;
 
     public Queue(){
 
     }
 
-    public void enqueue(E element){ //add element
+    public void enqueue(T Data){ //add element
+        QueueNode data = new QueueNode(Data);
+        if (first == null){ //if first
+            this.first = data;
+        }
+        else if(last == null){ //second thing
+            this.last = data;
+            data.setParent(first);
+        }
+        else { //more than 2 in line
+            data.setParent(last);
+            last=data;
+        }
 
     }
 
-    public E dequeue() { //remove and return the least recent element
+    public T dequeue() { //remove and return the least recent element
+        if(isEmpty()) throw new NoSuchElementException("Queue Underflow");
+        QueueNode<T> toReturn = first;
+        QueueNode<T> currNode = last;
+        for(int i = size-2; i > 0; i--){
+            currNode = last.getParent();
+        }
+        first = currNode;
+        currNode.setParent(null);
+        size--;
+        return toReturn.get();
+
 
     }
 
     public boolean isEmpty(){
-
+        return first == null;
     }
 
     public int size(){
-
+        return size;
     }
 
-    public E peek(){ //returns the least recent but does not remove
-
+    public T peek(){ //returns the least recent but does not remove
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        QueueNode <T> toReturn = first;
+        return toReturn.get();
     }
 }
--- src/QueueNode.java
+++ src/QueueNode.java
@@ -1,2 +1,65 @@
-public class QueueNode {
+public class QueueNode<T>{
+    //Generic datatype
+    private T t;
+    //Parent node
+    protected QueueNode parent;
+
+    //CONSTRUCTORS
+    //Constructor--default (set to null)
+    public QueueNode(){
+        this.t = null;
+        this.parent = null;
+    }
+    //Constructor--just data
+    public QueueNode(T t){
+        this.t = t;
+        this.parent = null;
+    }
+    //Constructor--data and parent/child nodes
+    public QueueNode(T t, QueueNode p, QueueNode c){
+        this.t = t;
+        this.parent = p;
+    }
+
+    //ACCESSORS
+    //Accessor--Stored data
+    public T get(){
+        return this.t;
+    }
+    //Accessor--Parent node
+    public QueueNode getParent(){
+        return this.parent;
+    }
+
+    //MUTATORS
+    //Mutator--Stored data
+    public void set(T t){
+        this.t = t;
+    }
+    //Mutator--Parent node
+    public void setParent(QueueNode p) {
+        this.parent = p;
+        updateParent(p);
+    }
+    //Mutator--private version to update the parent of the child node when you use setChild
+    // prevents infinite loop --> stack overflow
+    private void updateParent(QueueNode c){
+        c.parent = this;
+    }
+    //Mutator--private version to update the child of the parent node when you use setParent
+    // prevents infinite loop --> stack overflow
+
+    //OVERRIDES OF DEFAULT METHODS
+    //.toString()--returns datatype's toString
+    @Override
+    public String toString(){
+        if(this.t == null){
+            return null;
+        }
+        return this.t.toString();
+    }
+    //.equals()--returns if this datatype equals other node's datatype
+    public boolean equals(QueueNode n){
+        return this.t.equals(n.get());
+    }
 }
